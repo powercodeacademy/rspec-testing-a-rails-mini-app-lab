cb50d7ece160dd89c8c9af8935514e96c5fd63fe
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-09-04 14:02:50 -0400

    Add shoulda matchers

--- Gemfile
+++ Gemfile
@@ -21,17 +21,18 @@ gem "puma", ">= 5.0"
 # gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: %i[ windows jruby ]
+gem "tzinfo-data", platforms: %i[windows jruby]
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "debug", platforms: %i[ mri windows ]
-  gem "rspec-rails", "~> 6.1"
+  gem "capybara"
+  gem "debug", platforms: %i[mri windows]
   gem "factory_bot_rails"
   gem "pry"
-  gem "rubocop", require: false
   gem "rspec"
-  gem "capybara"
+  gem "rspec-rails", "~> 6.1"
+  gem "rubocop", require: false
+  gem "shoulda-matchers", "~> 6.0"
 end
 
 group :development do
@@ -44,4 +45,3 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
+require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
+require "shoulda/matchers"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -32,7 +33,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
-    Rails.root.join('spec/fixtures')
+    Rails.root.join("spec/fixtures")
   ]
 
   # FactoryBot methods
@@ -65,4 +66,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
