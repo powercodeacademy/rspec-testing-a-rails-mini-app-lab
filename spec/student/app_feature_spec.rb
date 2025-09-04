7e01c07f61042f10053591a6cd59854387c3df5e
Kyle <kylegranthousel@gmail.com>
2025-09-04 16:00:34 -0400

    Do things just to do them

--- spec/student/app_feature_spec.rb
+++ spec/student/app_feature_spec.rb
@@ -5,16 +5,35 @@
 require 'rails_helper'
 
 RSpec.describe "New User", type: :system do
+  before do
+    visit "/users/new"
+    let(:user) { build(:user) }
+  end
 
   it "loads the page" do
-    visit "/users/new"
     expect(page).to have_content("New User")
   end
   it "allows a user to create user" do
-    visit "/users/new"
-    fill_in "Name", with: "Nihar Patel"
-    fill_in "Email", with: "nkp52@hotmail.com"
-    click_button "Sign Up"
+    fill_in "Name", with: user.name
+    fill_in "Email", with: user.email
+    click_button "Submit"
+    expect(page).to have_content("Account created")
+  end
+end
+
+RSpec.describe "New Post", type: :system do
+  before do
+    visit "/posts/new"
+    subject(:post) { build(:post) }
+  end
+
+  it "loads the page" do
+    expect(page).to have_content("New Post")
+  end
+  it "allows a user to create a post" do
+    fill_in "Title", with: post.title
+    fill_in "Body", with: post.body
+    click_button "Submit"
     expect(page).to have_content("Account created")
   end
 end
