ec5fbc335289c4c0ce3cc420134e2b135c079aa7
Kyle <kylegranthousel@gmail.com>
2025-09-04 15:50:51 -0400

    Get feature spec half working

--- spec/student/app_feature_spec.rb
+++ spec/student/app_feature_spec.rb
@@ -2,3 +2,19 @@
 # Write your feature spec(s) here. This should test user-facing behavior using Capybara.
 
 # Use describe and it blocks as shown in the lessons.
+require 'rails_helper'
+
+RSpec.describe "New User", type: :system do
+
+  it "loads the page" do
+    visit "/users/new"
+    expect(page).to have_content("New User")
+  end
+  it "allows a user to create user" do
+    visit "/users/new"
+    fill_in "Name", with: "Nihar Patel"
+    fill_in "Email", with: "nkp52@hotmail.com"
+    click_button "Sign Up"
+    expect(page).to have_content("Account created")
+  end
+end
