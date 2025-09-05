6021fd61592dc03cd4b39d382e2f0c85ad7492d7
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-05 12:01:20 -0400

    Set up Post FactoryBot

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,1 +1,6 @@
-# (intentionally left blank for students to implement the Post factory)
+FactoryBot.define do
+  factory :post do
+    title { "Test Post" }
+    body { "Lorem ipsum etc. etc." }
+  end
+end
