5fdc45bfbb38e43fdca5d14b2508c11f1b238101
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-05 12:03:02 -0400

    Test has_many posts

--- spec/student/user_spec.rb
+++ spec/student/user_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
+  it { should have_many(:posts).dependent(:destroy) }
 
   it "is valid with valid attributes" do 
     user = FactoryBot.build(:user)
@@ -21,10 +22,19 @@ RSpec.describe User, type: :model do
   end
 
   it "is invalid with a duplicate email" do 
-    existing_user = FactoryBot.create(:user) 
-    duplicate_user = FactoryBot.build(:user) 
+    existing_user = FactoryBot.create(:user, email: "test@example.com") 
+    duplicate_user = FactoryBot.build(:user, email: "test@example.com") 
     
     expect(duplicate_user).not_to be_valid 
     expect(duplicate_user.errors[:email]).to include("has already been taken") 
+  end
+
+  it "can have many posts" do 
+    user = FactoryBot.create(:user) 
+    post1 = FactoryBot.create(:post, user: user) 
+    post2 = FactoryBot.create(:post, user: user) 
+    expect(user.posts).to contain_exactly(post1, post2) 
+    expect(post1.user).to eq(user) 
+    expect(post2.user).to eq(user) 
   end
 end
